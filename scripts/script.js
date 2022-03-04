47afe9ac4bc9b52540e259b04e7494f4038cc578
YuutaQwQ <90791480+YuutaQwQ@users.noreply.github.com>
2022-03-04 00:59:02 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+function search() {
+    window.location.href = "https://duckduckgo.com/?q=" + document.getElementById("search").value
+}
+
+function showTime() {
+	const date = new Date();
+
+	let today = date.toLocaleString("en", { weekday: "long" });
+	let hour = date.toLocaleString("pl", { hour: "2-digit" }); // use 24h time format
+	let minute = date.toLocaleString("en", { minute: "2-digit" });
+
+	minute = addZero(minute);
+
+	document.getElementById(
+		"date"
+	).innerHTML = `${today}, ${hour}:${minute}`;
+	setTimeout(showTime, 0);
+}
+
+function addZero(i) {
+	if (i.length < 2) i = "0" + i;
+	return i;
+}
+
+showTime();
